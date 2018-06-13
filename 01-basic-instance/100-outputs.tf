743224db2a438c4132f214d102d3e8f5f666007a
diodonfrost <keres@protonmail.com>
2018-06-13 09:45:27 +0200

    rename first example

---
+++ 01-basic-instance/100-outputs.tf
@@ -0,0 +1,11 @@
+# Display IP information
+
+# Internal address
+output "http_internal_address" {
+  value = "${google_compute_instance.http.network_interface.0.address}"
+}
+
+# External address
+output "http_external_address" {
+  value = "${google_compute_address.http.address}"
+}
