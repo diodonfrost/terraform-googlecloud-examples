f0916cbcb175c681936f441ae8f757146fe95cba
diodonfrost <keres@protonmail.com>
2018-06-13 09:46:54 +0200

    add network diagram

--- 01-sample-instance/provider.tf
+++
@@ -1,6 +0,0 @@
-# Configure the Google Cloud provider
-provider "google" {
-  credentials = "${file("gcp-terraform.json")}"
-  project     = "gcp-terraform-206213"
-  region      = "us-central1"
-}
--- 02-instance-with-volume/README.md
+++ 02-instance-with-volume/README.md
@@ -1,5 +1,7 @@
 # How to
 
+![infra instance-with-volume](../img/02-instance-with-volume.png "infra instance-with-volume")
+
 ### Create stack
 
 ```
